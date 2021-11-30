6f5bc9f77bfd1f9549065196ade5025293a61164
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2021-12-01 00:59:06 +0700

    Commit semua fitur untuk Survival Shooter

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+    public PlayerMovement playerMovement;
+    public PlayerShooting playerShooting;
+
+    Queue<Command> commands = new Queue<Command>();
+
+    void FixedUpdate() {
+        Command moveCommand = InputMovementHandling();
+        if (moveCommand != null) {
+            commands.Enqueue(moveCommand);
+            moveCommand.Execute();
+        }
+    }
+
+    void Update() {
+        Command shootCommand = InputShootHandling();
+        if (shootCommand != null) {
+            shootCommand.Execute();
+        }
+    }
+
+    Command InputMovementHandling() {
+        /*
+        if (Input.GetKey(KeyCode.D)) {
+            return new MoveCommand(playerMovement, 1, 0);
+        }
+        else if (Input.GetKey(KeyCode.A)) {
+            return new MoveCommand(playerMovement, -1, 0);
+        }
+        else if (Input.GetKey(KeyCode.W)) {
+            return new MoveCommand(playerMovement, 0, 1);
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            return new MoveCommand(playerMovement, 0, -1);
+        }*/
+
+        float h = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
+        float v = (Input.GetKey(KeyCode.S) ? -1 : 0) + (Input.GetKey(KeyCode.W) ? 1 : 0);
+
+        if ((h != 0) || (v != 0)) {
+            return new MoveCommand(playerMovement, h, v);
+        } else if (Input.GetKey(KeyCode.Z)) {
+            return Undo();
+        }
+        else {
+            return new MoveCommand(playerMovement, 0, 0); ;
+        }
+    }
+
+    Command Undo() {
+        if (commands.Count > 0) {
+            Command undoCommand = commands.Dequeue();
+            undoCommand.UnExecute();
+        }
+        return null;
+    }
+
+    Command InputShootHandling() {
+        if (Input.GetButton("Fire1")) {
+            return new ShootCommand(playerShooting);
+        }
+        else {
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+
+    [SerializeField]
+    MonoBehaviour factory = null;
+
+    IFactory Factory { get { return factory as IFactory; } }
+
+    void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnEnemy = Random.Range(0, 3);
+        int spawnPointIndex = Random.Range(0, 3);
+
+        Factory.FactoryMethod(spawnEnemy, spawnPointIndex);
+        //Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+}
